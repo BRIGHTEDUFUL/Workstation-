3f965b3c630727539aafccc3087188159ebdebba
Bright Eduful <brighteduful22@gmail.com>
2025-09-17 15:08:01 +0000

    Try fixing this error: `Runtime Error: Error: backNodeRef is not defined

--- src/components/design/design-header.tsx
+++ src/components/design/design-header.tsx
@@ -24,7 +24,7 @@ const DesignHeader = ({ cardDetails, cardFrontRef, cardBackRef }: DesignHeaderPr
     
     const handleExport = async () => {
         const frontNode = cardFrontRef.current;
-        const backNode = backNodeRef.current;
+        const backNode = cardBackRef.current;
 
         if (!frontNode || !backNode) {
             toast({
